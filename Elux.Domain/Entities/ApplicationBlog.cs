b73a9a228ddd41dd697526c4fddb9e480389e8b0
Vardan Mkrtchyan <vardanmkrtchyan171@gmail.com>
2024-12-09 20:25:07 +0400

    FixApplicationBlog

--- Elux.Domain/Entities/ApplicationBlog.cs
+++ Elux.Domain/Entities/ApplicationBlog.cs
@@ -6,6 +6,6 @@
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime ReleaseDate { get; set; }
-        public string Picture { get; set; }
+        public string Picture { get; set; }//asd//asd
     }
 }
