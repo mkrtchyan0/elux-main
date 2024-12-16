3cbe19054e520f9d94c9c9c849a71cbb62349a64
Paruyr <phambardzumyan@gmail.com>
2024-12-16 18:31:13 +0400

    AddExpertCommand

--- Elux.Application/Commands/Expert/AddExpertCommand.cs
+++ Elux.Application/Commands/Expert/AddExpertCommand.cs
@@ -1,16 +1,12 @@
 ﻿using Elux.Domain.Entities;
 using Elux.Domain.Responses;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Elux.Application.Commands.Expert
 {
     public class AddExpertCommand : IRequest<BaseResponse<ApplicationExpert>>
     {
+        //AddExpertCommand
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Proffesion { get; set; }
